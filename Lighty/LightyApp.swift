b07f7fa2a05cd85ec2f734904ce35deec3f9b7f4
Donovan Efrain Barrientos Abarca <donovan_ba@outlook.com>
2026-02-05 17:31:26 -0600

    Initial Commit

---
+++ Lighty/LightyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightyApp.swift
+//  Lighty
+//
+//  Created by Donovan Efrain Barrientos Abarca on 05/02/26.
+//
+
+import SwiftUI
+
+@main
+struct LightyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
